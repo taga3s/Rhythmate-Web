0f85f378bd27ef133148691584c2d2e0ee77d83c
seiya_tagami <bepozurapu274@gmail.com>
2024-02-19 13:30:28 +0900

    update: スタイルを修正

--- src/features/common/components/Menu.tsx
+++ src/features/common/components/Menu.tsx
@@ -4,7 +4,7 @@ import { AnalyticsIcon, ManageIcon, ProfileIcon, QuestsIcon } from "./icons";
 
 export const Menu = () => {
   return (
-    <ul className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+    <ul className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
       <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
         <MenuButton name="Quests" icon={<QuestsIcon />} />
         <MenuButton name="Manage" icon={<ManageIcon />} />
--- src/features/common/components/MenuButton.tsx
+++ src/features/common/components/MenuButton.tsx
@@ -7,14 +7,9 @@ type Props = {
 export const MenuButton: FC<Props> = (props) => {
   const { name, icon } = props;
   return (
-    <button
-      type="button"
-      className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-    >
+    <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group">
       {icon}
-      <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-        {name}
-      </span>
+      <span className="text-sm text-gray-500 group-hover:text-blue-600">{name}</span>
     </button>
   );
 };
