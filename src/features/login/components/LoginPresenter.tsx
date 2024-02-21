0f966e32dcd1d028ed996e62f28f68f886badfd9
164cm54kg <mamedragon2020@gmail.com>
2024-02-21 10:02:33 +0900

    update 修正しました

--- src/features/login/components/LoginPresenter.tsx
+++ src/features/login/components/LoginPresenter.tsx
@@ -21,12 +21,12 @@ export const LoginPresenter = () => {
         viewBox="0 0 24 24"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
-      <form onSubmit={handleSubmit(onsubmit)}>
+      <form className="gap-4 flex flex-col items-center" onSubmit={handleSubmit(onsubmit)}>
         <div className="flex flex-col gap-2">
           <label htmlFor="email">ユーザーネーム</label>
           <input type="email" id="email" className="rounded-full border-2 w-52 h-128 p-3" />
@@ -36,15 +36,15 @@ export const LoginPresenter = () => {
           <label htmlFor="password">パスワード</label>
           <input type="password" id="password" className="rounded-full border-2 w-52 h-128 p-3" />
         </div>
-        <div className="flex">
-          <p>新しく始めますか？</p>
+        <span>
+          新しく始めますか？
           <a href="#" className="text-blue-600">
             新規登録
           </a>
-        </div>
+        </span>
         <button
           type="submit"
-          className="my-5 p-10 w-48 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="p-10 mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
         >
           ログインする
         </button>
