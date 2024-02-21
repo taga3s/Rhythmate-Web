0773205823f224975106a73dbedad3c0176c7bf5
mamenz752 <mamenz752@gmail.com>
2024-02-21 22:21:20 +0900

    feat: ユーザー情報変更モーダルの実装

--- src/features/profile/components/ProfilePresenter.tsx
+++ src/features/profile/components/ProfilePresenter.tsx
@@ -1,25 +1,24 @@
+import { useState } from "react";
+import { ProfileUserSettingsModalButton } from "./ProfileUserSettingsModalButton";
+import { ProfileUserSettingsModal } from "./ProfileUserSettingModal";
+
 export const ProfilePresenter = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
       <div className="my-5 flex flex-col items-center gap-5">
         <div className="w-[300px] p-5 bg-white border border-gray-200 rounded-lg shadow">
           <div className="flex justify-end">
-            {/* モーダル実装 */}
-            <button>
-              <svg
-                className="w-[24px] h-[24px] text-gray-800"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5 8a4 4 0 1 1 7.8 1.3l-2.5 2.5A4 4 0 0 1 5 8Zm4 5H7a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h2.2a3 3 0 0 1-.1-1.6l.6-3.4a3 3 0 0 1 .9-1.5L9 13Zm9-5a3 3 0 0 0-2 .9l-6 6a1 1 0 0 0-.3.5L9 18.8a1 1 0 0 0 1.2 1.2l3.4-.7c.2 0 .3-.1.5-.3l6-6a3 3 0 0 0-2-5Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
+            <ProfileUserSettingsModalButton onClickFn={toggleModal} />
           </div>
           <div className="flex justify-between gap-4">
             <div>
@@ -81,6 +80,7 @@ export const ProfilePresenter = () => {
           </div>
         </ul>
       </div>
+      {isModalOpen && <ProfileUserSettingsModal onClickFn={closeModal} />}
     </div>
   );
 };
