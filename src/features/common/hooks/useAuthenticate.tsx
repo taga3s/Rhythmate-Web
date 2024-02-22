3a622fb5a393982192c0329624952395e6f109a5
seiya_tagami <bepozurapu274@gmail.com>
2024-02-22 23:21:13 +0900

    fix: useAuthenticateのエラーを修正

--- src/features/common/hooks/useAuthenticate.tsx
+++ src/features/common/hooks/useAuthenticate.tsx
@@ -2,7 +2,7 @@ import { useCookies } from "react-cookie";
 
 export const useAuthenticate = () => {
   const [cookies] = useCookies(["access_token"]);
-  const accessToken: string = cookies.access_token;
+  const accessToken: string = cookies.access_token ?? "";
   return {
     isAuthenticated: accessToken.length !== 0,
   };
