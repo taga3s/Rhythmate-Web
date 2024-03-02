8286182cfe85c02a3fffe54d5cc7ccf713310d96
seiya_tagami <bepozurapu274@gmail.com>
2024-03-02 18:23:14 +0900

    fix: 編集機能の修正

--- src/features/quests/api/types.ts
+++ src/features/quests/api/types.ts
@@ -1,3 +1,4 @@
+export type Difficulty = "EASY" | "NORMAL" | "HARD";
 export type QuestState = "INACTIVE" | "ACTIVE";
 
 export type ListResponse = {
@@ -10,7 +11,7 @@ export type ListResponse = {
     started_at: string;
     minutes: number;
     tag_id: string;
-    difficulty: string;
+    difficulty: Difficulty;
     state: QuestState;
     is_succeeded: boolean;
     continuation_level: number;
@@ -39,7 +40,7 @@ export type StartResponse = {
   started_at: string;
   minutes: number;
   tag_id: string;
-  difficulty: string;
+  difficulty: Difficulty;
   state: QuestState;
   is_succeeded: boolean;
   continuation_level: number;
@@ -67,7 +68,7 @@ export type FinishResponse = {
   started_at: string;
   minutes: number;
   tag_id: string;
-  difficulty: string;
+  difficulty: Difficulty;
   state: QuestState;
   is_succeeded: boolean;
   continuation_level: number;
@@ -95,7 +96,7 @@ export type ForceFinishResponse = {
   started_at: string;
   minutes: number;
   tag_id: string;
-  difficulty: string;
+  difficulty: Difficulty;
   state: QuestState;
   is_succeeded: boolean;
   continuation_level: number;
