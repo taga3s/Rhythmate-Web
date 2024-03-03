50f72ce91a50a1177e6c6190c5e1bc86a6954caa
seiya_tagami <bepozurapu274@gmail.com>
2024-03-03 20:24:32 +0900

    fix: constantの修正

--- src/features/manage/common/constant/constant.ts
+++ src/features/manage/common/constant/constant.ts
@@ -1,4 +1,4 @@
 import { Difficulty } from "../../api/types";
 
-export const DATES = ["日", "月", "火", "水", "木", "金", "土"];
+export const DATES = ["月", "火", "水", "木", "金", "土", "日"];
 export const DIFFICULTIES: Difficulty[] = ["EASY", "NORMAL", "HARD"];
