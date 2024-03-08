9ba83eb8d7cc44a538b47e9d6fe0588a23eab0e4
seiya_tagami <bepozurapu274@gmail.com>
2024-03-08 20:34:38 +0900

    refac: weekly-report-apiのフォルダ以降完了

--- src/features/analytics/api/hooks/useQueryWeeklyReport.ts
+++ src/features/analytics/api/hooks/useQueryWeeklyReport.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { WeeklyReport } from "../model";
 import { FetchError } from "../../../../pkg/api/util/fetchError";
-import { createFactory } from "../factory";
+import { WeeklyReport } from "../../../../api/weeklyReport/model";
+import { createFactory } from "../../../../api/weeklyReport/factory";
 
 export const useQueryWeeklyReports = () => {
+  const weeklyReportFactory = createFactory();
   return useQuery<WeeklyReport[], FetchError>({
     queryKey: ["weeklyReports"],
     queryFn: async () => {
-      const weeklyReports = await createFactory().listWeeklyReports();
+      const weeklyReports = await weeklyReportFactory.listWeeklyReports();
       return weeklyReports;
     },
     staleTime: Infinity,
