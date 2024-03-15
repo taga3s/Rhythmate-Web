27ceb365b8106b14501fb7d6a4ae6e901fc8c6ed
mamenz752 <mamenz752@gmail.com>
2024-03-15 15:52:58 +0900

    fix: build時エラーの修正

--- src/features/profile/badges/components/badge/BadgeItem.tsx
+++ src/features/profile/badges/components/badge/BadgeItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { Bow } from "./badgeItems/Bow";
 import { Cat } from "./badgeItems/Cat";
 import { Crown } from "./badgeItems/Crown";
