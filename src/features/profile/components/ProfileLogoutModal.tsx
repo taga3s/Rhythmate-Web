7c3f4046212ba66245378ab9c4bbe67436e6bdd6
Mao <mamedragon2020@gmail.com>
2024-03-20 21:28:14 +0900

    fix:ログアウトモーダル修正

--- src/features/profile/components/ProfileLogoutModal.tsx
+++ src/features/profile/components/ProfileLogoutModal.tsx
@@ -15,7 +15,7 @@ export const ProfileLogoutModal: FC<Props> = ({ onClickFn }) => {
       <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 z-50" onClick={onClickFn} />
       <div
         aria-hidden="true"
-        className="overflow-y-auto overflow-x-hidden flex justify-center items-center z-[100] max-h-full fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        className="overflow-y-auto overflow-x-hidden flex justify-center items-center z-[100] max-h-full fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full"
       >
         <div className="relative p-4 w-full max-w-md max-h-full">
           {/* <!-- Modal content --> */}
