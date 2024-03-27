b8e6386ca7e0681881393c160a6555941f18507e
mamenz752 <mamenz752@gmail.com>
2024-03-27 17:26:56 +0900

    fix: タグ新規作成ボタンの修正

--- src/features/manage/tags/components/TagsNewButton.tsx
+++ src/features/manage/tags/components/TagsNewButton.tsx
@@ -8,15 +8,16 @@ export const TagsNewButton: FC<Props> = ({ onClickFn }) => {
   return (
     <button
       onClick={onClickFn}
-      className="flex justify-center items-center bg-rhyth-light-blue h-8 w-8 p-1 rounded-sm right-12 bottom-24 shadow-sm"
+      className="flex justify-between items-center gap-1 bg-rhyth-light-blue h-8 w-auto p-2 rounded-sm right-12 bottom-24 shadow-sm"
     >
+      <span className="flex items-center text-white tracking-wider">新規タグ作成</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth="1.5"
         stroke="white"
-        className="w-10 h-10"
+        className="w-6 h-6"
       >
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
       </svg>
