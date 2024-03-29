dabbe2e017e3af44c11ddeec51fa7d579c0b7178
mamenz752 <mamenz752@gmail.com>
2024-03-30 01:22:23 +0900

    fix: クエストがない状態のQuestBoardを変更

--- src/features/quests/components/QuestsPresenter.tsx
+++ src/features/quests/components/QuestsPresenter.tsx
@@ -50,6 +50,26 @@ export const QuestsPresenter = () => {
         </LoadingContainer>
       ) : (
         <>
+          <div className="flex gap-2 my-2">
+            <svg
+              className="w-6 h-6 text-rhyth-gray"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 12H5m14 0-4 4m4-4-4-4"
+              />
+            </svg>
+            <h1 className="font-cp-font text-rhyth-gray tracking-widest">NEXT CHALLENGE</h1>
+          </div>
           <div className="bg-white">
             {currentQuest ? <QuestBoard currentQuest={currentQuest} /> : <QuestBoardNoData />}
           </div>
