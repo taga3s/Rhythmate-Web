53e57765b87bc5a808839b4eae2db382483aec9f
seiya_tagami <bepozurapu274@gmail.com>
2024-03-29 13:26:29 +0900

    wip: vite.config.tsにpwaの設定を追記

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      injectRegister: "auto",
+      manifest: {
+        name: "Rhythmate",
+        short_name: "Rhythmate",
+        theme_color: "#D9D9D9",
+        icons: [
+          {
+            src: "/icons/rhyth-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/rhyth-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
