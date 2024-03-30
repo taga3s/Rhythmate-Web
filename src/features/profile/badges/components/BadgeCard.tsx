63e5826363a8ea16e3380b4a8d7dbc507a9ebef8
dyoshyy <d.yoshhyy@gmail.com>
2024-03-30 11:41:24 +0900

    feat: dbの値に応じたバッジの表示を実装

--- src/features/profile/badges/components/BadgeCard.tsx
+++ src/features/profile/badges/components/BadgeCard.tsx
@@ -1,24 +1,29 @@
 import { FC } from "react";
+import { Badge } from "./badge/Badge";
 
 type Props = {
   id: string;
   badgeId: string;
+  name: string;
+  description: string;
+  imageDir: string;
   obtainedAt: string;
   isPinned: boolean;
 };
 
 export const BadgeCard: FC<Props> = (props) => {
-  const { id, badgeId, obtainedAt, isPinned } = props;
-
+  const { id, badgeId, name, description, imageDir, obtainedAt, isPinned } = props;
   return (
-    <div className="w-full h-auto  bg-white border border-[#AAAAAA] border-solid rounded-lg shadow">
-      <div className="grid grid-flow-row-dense grid-cols-3 gap-3 p-4" key={badgeId}>
-        <div className="col-span-1">
-          <img src={id} alt={badgeId} />
+    <div className="w-full h-full bg-white border border-[#AAAAAA] border-solid rounded-lg shadow">
+      <div className="h-32 grid grid-flow-row-dense grid-cols-3 gap-3 p-2" key={badgeId}>
+        <div className="h-auto col-span-1">
+          {/* <img src={imageDir} alt={badgeId} /> */}
+          <Badge imageType={imageDir} flameClassName="" sparklingClassName="" itemClassName="" />
         </div>
         <div className="flex flex-col col-span-2 ">
           <div className="flex justify-end">達成日: {obtainedAt}</div>
-          <div className="flex items-center text-xl">{badgeId}</div>
+          <div className="flex items-center text-xl">{name}</div>
+          <div className="flex items-center text-xl">{description}</div>
         </div>
       </div>
     </div>
--- src/features/profile/badges/components/BadgesPresenter.tsx
+++ src/features/profile/badges/components/BadgesPresenter.tsx
@@ -1,11 +1,11 @@
 import { BadgesBackButton } from "./BadgesBackButton";
 import { useNavigate } from "@tanstack/react-router";
 
-const badgeFrame: string = "/badges/frame.png";
+// const badgeFrame: string = "/badges/frame.png";
 
 // 仮のデータ
 // import { achievementData } from "../constant/badges";
-import { badges } from "../constant/badges";
+// import { badges } from "../constant/badges";
 
 import { useQueryBadgeList } from "../api/badge/hooks/useQueryBadge";
 import { BadgeCard } from "./BadgeCard";
@@ -23,43 +23,28 @@ export const BadgesPresenter = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center w-full mx-auto">
+      <div className="flex flex-col items-center w-full">
         <div className="flex flex-col space-y-5 w-full">
           <div className="flex justify-start gap-3">
             <BadgesBackButton onClickFn={navigationToProfile} />
           </div>
           <p className="flex text-2xl justify-center font-bold">獲得バッジ一覧</p>
         </div>
-        {/* <table className="mt-6 border mx-auto">
-          <tbody>
-            {badgeList?.map((badge, index) => (
-              <tr className="grid grid-flow-row-dense grid-cols-3 gap-3 p-4" key={index}>
-                <td className="col-span-1">
-                  <img src={achievementData[index].isAchieved === true ? badge.image : badgeFrame} alt={badge.text} />
-                </td>
-                <td className="flex flex-col col-span-2 ">
-                  <div className="flex justify-end">
-                    達成日時:
-                    {achievementData[index].isAchieved === true ? achievementData[index].achievementDate : "未達成"}
-                  </div>
-                  <div className="flex items-center text-xl">{badge.text}</div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
         {isLoading ? (
           <LoadingContainer>
             <Loading />
           </LoadingContainer>
         ) : (
-          <ul className="mt-4 flex flex-col items-center gap-6">
+          <ul className="flex flex-col items-center mt-3 gap-3 w-full">
             {badgeList?.map((badge) => {
               return (
                 <BadgeCard
                   key={badge.id}
                   id={badge.id}
                   badgeId={badge.badgeId}
+                  name={badge.name}
+                  description={badge.description}
+                  imageDir={badge.imageDir}
                   isPinned={badge.isPinned}
                   obtainedAt={badge.obtainedAt}
                 />
