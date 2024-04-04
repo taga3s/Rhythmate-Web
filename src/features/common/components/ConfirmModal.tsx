b40a063c598fb561369a2ab7ff9218585eee77a7
mamenz752 <mamenz752@gmail.com>
2024-04-04 21:32:20 +0900

    fix: ConfirmModalの修正

--- src/features/common/components/ConfirmModal.tsx
+++ src/features/common/components/ConfirmModal.tsx
@@ -60,7 +60,7 @@ export const ConfirmModal: FC<Props> = (props) => {
             <button
               type="submit"
               onClick={closeModal}
-              className="w-full text-white bg-rhyth-black hover:bg-[#222222] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              className="w-full text-white bg-rhyth-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               {cancelBtnText}
             </button>
