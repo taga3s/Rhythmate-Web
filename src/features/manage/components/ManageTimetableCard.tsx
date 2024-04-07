916f29c9d829794b83bf3436c1e3bd4a1a4e6164
mamenz752 <mamenz752@gmail.com>
2024-04-07 20:23:42 +0900

    fix: メモがない場合の表示修正

--- src/features/manage/components/ManageTimetableCard.tsx
+++ src/features/manage/components/ManageTimetableCard.tsx
@@ -124,7 +124,30 @@ export const ManageTimetableCard: FC<Props> = (props) => {
                   </button>
                 </div>
                 <h3 className="text-md ml-2 font-bold text-rhyth-dark-blue">
-                  {props.description === "" ? "サンプルメモ" : props.description}
+                  {props.description === "" ? (
+                    <div className="flex items-center gap-1">
+                      <svg
+                        className="w-6 h-6 text-rhyth-dark-blue"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
+                        />
+                      </svg>
+                      メモを入力できます
+                    </div>
+                  ) : (
+                    props.description
+                  )}
                 </h3>
               </div>
               <div className="mt-4">
