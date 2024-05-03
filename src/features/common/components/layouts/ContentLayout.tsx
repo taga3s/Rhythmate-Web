c919a3ba9339ea26aeb5bf87b6e6be1f139a1a4a
Mao <mamedragon2020@gmail.com>
2024-05-03 23:30:44 +0900

    fix:画像URLの修正

--- src/features/common/components/layouts/ContentLayout.tsx
+++ src/features/common/components/layouts/ContentLayout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const ContentLayout: FC<Props> = ({ children }) => {
   return (
-    <div className="bg-[url('public/2278346copy.jpg')] bg-cover w-full min-h-screen">
+    <div className="bg-[url('public/background.jpg')] bg-cover w-full min-h-screen">
       <div className="max-w-[680px] mx-auto px-5 pt-24 pb-28">{children}</div>
     </div>
   );
