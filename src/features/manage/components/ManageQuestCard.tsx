c318072e2e93613cd6ec8ea8a64b3445578b5443
seiya_tagami <bepozurapu274@gmail.com>
2024-05-04 15:23:53 +0900

    refac: クエスト管理カードのUIの再度修正

--- src/features/manage/components/ManageQuestCard.tsx
+++ src/features/manage/components/ManageQuestCard.tsx
@@ -5,6 +5,7 @@ import { ClockIcon } from "../../common/components/icons/ClockIcon";
 import { calcExp } from "../../common/funcs/calcExp";
 import { convertEnToJPWeekday } from "../common/funcs";
 import { ManageProgressBar } from "./ManageProgressBar";
+import { formatDateTimeOnlyTime } from "../../../pkg/util/dayjs";
 
 const convertENToJPWeekdayString = (weekDays: Day[]) => {
   const result = weekDays.map((day) => convertEnToJPWeekday(day)).join("・");
@@ -49,14 +50,14 @@ export const ManageQuestCard: FC<Props> = (props) => {
   };
 
   return (
-    <li className="w-full h-auto bg-white border-2 border-rhyth-light-gray border-solid rounded-lg shadow-lg">
-      <div className="flex items-center p-4">
-        <span className="font-bold text-xl text-rhyth-dark-blue">{props.title}</span>
-      </div>
+    <li className="w-full h-auto bg-white rounded-xl shadow-lg">
       <div>
-        <hr className="h-0.5 bg-rhyth-light-gray" />
         <div className="p-2">
-          <div className="flex items-center justify-between">
+          <div className="p-2">
+            <span className="font-cp-font font-bold text-2xl text-rhyth-dark-blue">{props.title}</span>
+          </div>
+          <hr className="h-0.5 bg-rhyth-light-gray" />
+          <div className="flex items-center justify-between mt-2">
             <button
               type="button"
               className="p-1 ml-auto"
@@ -67,7 +68,7 @@ export const ManageQuestCard: FC<Props> = (props) => {
                 })
               }
             >
-              <div className="flex items-center justify-center gap-1 text-rhyth-blue hover:text-rhyth-hover-blue font-bold">
+              <div className="flex items-center justify-center gap-1 text-rhyth-dark-blue font-bold">
                 <svg
                   className="w-6 h-6"
                   aria-hidden="true"
@@ -92,8 +93,8 @@ export const ManageQuestCard: FC<Props> = (props) => {
               </div>
             </button>
           </div>
-          <h3 className="text-md font-bold text-rhyth-dark-blue">
-            <div className="flex items-center gap-1">
+          <h3 className="text-md text-rhyth-dark-blue">
+            <div className="flex items-center gap-2">
               <svg
                 className="w-6 h-6 text-rhyth-dark-blue"
                 aria-hidden="true"
@@ -115,9 +116,12 @@ export const ManageQuestCard: FC<Props> = (props) => {
             </div>
           </h3>
           <div className="flex justify-between items-center mt-2">
-            <div className="flex items-center">
-              <ClockIcon color="text-rhyth-blue" />
-              <h3 className="my-2 ml-2 text-md font-bold text-rhyth-blue">{convertENToJPWeekdayString(props.days)}</h3>
+            <div className="flex justify-start gap-2">
+              <ClockIcon color="text-rhyth-dark-blue" />
+              <div className="flex flex-col items-start text-rhyth-dark-blue">
+                <span>{formatDateTimeOnlyTime(props.startsAt)}～</span>
+                <span>{convertENToJPWeekdayString(props.days)}</span>
+              </div>
             </div>
             {props.tagName !== undefined ? (
               <div
@@ -147,11 +151,11 @@ export const ManageQuestCard: FC<Props> = (props) => {
         <div className="flex items-center h-28">
           <div className=" w-full h-full p-2">
             <div className="flex my-2">
-              <p className="font-cp-font text-rhyth-green mt-auto ml-1">継続レベル</p>
+              <p className="mt-auto ml-1 font-bold text-rhyth-green">継続レベル</p>
             </div>
             <ManageProgressBar level={props.continuationLevel} />
             <div className="flex justify-end items-center text-sm">
-              <p className="font-cp-font tracking-[0.2em] text-white mt-1 bg-rhyth-orange px-2 py-1 rounded-full">
+              <p className="font-cp-font tracking-[0.2em] text-white font-bold bg-rhyth-orange mt-1 px-2 py-1 rounded-full">
                 BONUS
               </p>
               <span className="ml-1 font-medium text-md text-rhyth-orange tracking-wider">&times;</span>
@@ -160,8 +164,8 @@ export const ManageQuestCard: FC<Props> = (props) => {
               </span>
             </div>
           </div>
-          <div className="bg-rhyth-red min-w-20 h-full flex flex-col justify-center items-center gap-2 font-cp-font text-[12px] tracking-wider rounded-br-md">
-            <p className="text-white font-semibold">獲得Exp</p>
+          <div className="bg-rhyth-red min-w-20 h-full flex flex-col justify-center items-center gap-2 font-cp-font text-[12px] tracking-wider rounded-br-lg">
+            <span className="text-white font-semibold font-cp-font">獲得Exp</span>
             <div className="flex justify-between items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
