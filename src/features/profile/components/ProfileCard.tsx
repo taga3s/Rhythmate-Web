6b06957caa5459c63c1f9fe8a4f829c82a72a41d
Mao <mamedragon2020@gmail.com>
2024-05-05 20:54:40 +0900

    feat:プロフィール背景画像の設定

--- src/features/profile/components/ProfileCard.tsx
+++ src/features/profile/components/ProfileCard.tsx
@@ -11,28 +11,28 @@ type Props = {
 export const ProfileCard: FC<Props> = ({ userName, imageUrl, currentLevel }) => {
   const { data: badgeList } = useQueryBadgeList();
   const pinnedBadgeList = badgeList?.filter((badge) => badge.isPinned) ?? [];
-
   return (
-    <div className="w-screen pt-16 -mt-16 p-5 bg-rhyth-light-blue  bg-left border border-gray-200 rounded-b-lg shadow">
-      <div className="flex justify-between items-center gap-4 box-border">
-        <div className="w-20 md:w-32 h-20 md:h-32">
-          <img src={imageUrl} alt="プロフィール画像" className="w-full h-full rounded-full" />
+    <div className="w-screen justify-center pt-24 -mt-24 pb-8 bg-white bg-cover bg-[url('public/bg-geometric2.png')] border border-gray-200 rounded-b-lg shadow">
+      <div className="flex px-4 max-w-[680px] mx-auto justify-between items-center box-border">
+        <div className="w-36 md:w-32 h-36 md:h-32">
+          <img src={imageUrl} alt="プロフィール画像" className="w-full h-auto rounded-full" />
         </div>
-        <p className="text-3xl font-extrabold text-rhyth-dark-blue">{userName}</p>
-      </div>
-      <div className="flex justify-between mt-4">
-        <div className="flex items-end gap-7 text-white bg-rhyth-light-blue focus:outline-none focus:ring-4 font-extrabold text-3xl px-3 py-2 mb-2 -ml-5">
-          <div className="text-base">Lv. </div>
-          <div>{currentLevel}</div>
-        </div>
-        <div className="flex justify-end w-2/3 gap-3">
-          {pinnedBadgeList?.map((badge) => {
-            return (
-              <div className="flex h-full w-1/3" key={badge.id}>
-                <Badge imageType={badge.imageType} frameColor={badge.frameColor} />
-              </div>
-            );
-          })}
+        <div className="flex flex-col">
+          <p className="text-4xl font-extrabold text-right text-black">{userName}</p>
+          <div className="flex justify-end items-end gap-3 focus:outline-none focus:ring-4 font-extrabold text-2xl text-black">
+            <div className="text-base">Lv. </div>
+            <div>{currentLevel}</div>
+          </div>
+
+          <div className="flex justify-end w-2/3 gap-3">
+            {pinnedBadgeList?.map((badge) => {
+              return (
+                <div className="flex h-full w-1/3" key={badge.id}>
+                  <Badge imageType={badge.imageType} frameColor={badge.frameColor} />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
