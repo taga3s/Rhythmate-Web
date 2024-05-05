e7658a18239d45caf41a1f4a26523e1fb6c1bc2f
dyoshyy <d.yoshhyy@gmail.com>
2024-05-05 22:45:33 +0900

    update: authの型の置き換え

--- src/api/tag/type.ts
+++ src/api/tag/type.ts
@@ -1,58 +1,5 @@
 import * as schemaHelper from "../../Rhythmate-Service/src/pkg/schemaHelper";
 
-// type TagBaseResponse = {
-//   id: string;
-//   name: string;
-//   color: string;
-// };
-
-// export type ListTagsResponse = {
-//   status: string;
-//   tags: TagBaseResponse[];
-// };
-
-// export type CreateTagParams = {
-//   name: string;
-//   color: string;
-// };
-
-// export type CreateRequest = {
-//   name: string;
-//   color: string;
-// };
-
-// export type CreateResponse = TagBaseResponse & {
-//   status: string;
-// };
-
-// export type DeleteTagParams = {
-//   id: string;
-// };
-
-// export type DeleteRequest = {
-//   id: string;
-// };
-
-// export type DeleteResponse = {
-//   status: string;
-// };
-
-// export type UpdateTagParams = {
-//   id: string;
-//   name: string;
-//   color: string;
-// };
-
-// export type UpdateRequest = {
-//   id: string;
-//   name: string;
-//   color: string;
-// };
-
-// export type UpdateResponse = TagBaseResponse & {
-//   status: string;
-// };
-
 export type CreateTagParams = schemaHelper.RequestData<"/tags", "post">;
 
 export type CreateTagRequest = schemaHelper.RequestData<"/tags", "post">;
