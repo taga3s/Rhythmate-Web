0f71b8be75e77fa29dc607c25267f2e46a225d75
taga3s <bepozurapu274@gmail.com>
2024-08-25 11:01:12 +0900

    fix: header ui logic

--- src/features/common/components/header/HeaderManageButtonGroup.tsx
+++ src/features/common/components/header/HeaderManageButtonGroup.tsx
@@ -2,12 +2,11 @@ import { useNavigate } from "@tanstack/react-router";
 import { useSetSearchModalIsOpen } from "../../contexts/searchModalIsOpenContext";
 import { HeaderBaseButton } from "./HeaderBaseButton";
 
-export const HeaderManageButton = () => {
+export const HeaderManageButtonGroup = () => {
   const setSearchModalIsOpen = useSetSearchModalIsOpen();
   const navigation = useNavigate();
-
   return (
-    <>
+    <div className="flex items-center justify-end">
       <HeaderBaseButton icon="Tag" onClickFn={() => navigation({ to: "/manage/tags" })} />
       <HeaderBaseButton
         icon="Search"
@@ -15,6 +14,6 @@ export const HeaderManageButton = () => {
           setSearchModalIsOpen(true);
         }}
       />
-    </>
+    </div>
   );
 };
