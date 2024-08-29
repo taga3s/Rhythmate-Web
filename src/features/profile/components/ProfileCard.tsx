6378410222a30cf6f25272dcbce3bc8e52e1cdf8
taga3s <bepozurapu274@gmail.com>
2024-08-29 23:19:53 +0900

    fix: badge input values and ui

--- src/features/profile/components/ProfileCard.tsx
+++ src/features/profile/components/ProfileCard.tsx
@@ -14,8 +14,8 @@ export const ProfileCard: FC<Props> = ({ userName, imageUrl, currentLevel }) => 
 
   return (
     // <div className="w-screen justify-center pt-24 -mt-24 pb-8 bg-white bg-cover bg-[url('/bg-geometric2.png')] border border-gray-200 shadow">
-    <div className="w-full justify-center py-8 bg-white bg-cover border rounded-lg border-gray-200 shadow">
-      <div className="flex px-4 md:px-8 justify-between items-center box-border">
+    <div className="w-full justify-center p-4 bg-white bg-cover border rounded-lg border-gray-200 shadow">
+      <div className="flex md:px-8 pt-8 pb-4 justify-between items-center box-border">
         <div className="w-32 h-32">
           <img src={imageUrl} alt="プロフィール画像" className="w-full h-auto rounded-full" />
         </div>
@@ -25,16 +25,23 @@ export const ProfileCard: FC<Props> = ({ userName, imageUrl, currentLevel }) => 
             <div className="text-base">Lv. </div>
             <div>{currentLevel}</div>
           </div>
-          <div className="flex justify-end w-2/3 gap-3">
+        </div>
+      </div>
+      <div>
+        {pinnedBadgeList.length > 0 && (
+          <div className="w-full flex justify-end">
             {pinnedBadgeList?.map((badge) => {
               return (
-                <div className="flex h-full w-1/3" key={badge.id}>
-                  <Badge imageType={badge.imageType} frameColor={badge.frameColor} />
+                <div className="flex flex-col gap-2" key={badge.id}>
+                  <div className="h-12">
+                    <Badge imageType={badge.imageType} frameColor={badge.frameColor} />
+                  </div>
+                  <span className="text-xs">{badge.name}</span>
                 </div>
               );
             })}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
