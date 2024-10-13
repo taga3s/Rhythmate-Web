96f431e2092e7ace2df36c0dc3282d90e44f49df
taga3s <bepozurapu274@gmail.com>
2024-10-13 17:32:06 +0900

    fix questList ui

--- src/features/quests/components/QuestListNoData.tsx
+++ src/features/quests/components/QuestListNoData.tsx
@@ -7,28 +7,26 @@ type Props = {
 
 export const QuestListNoData: FC<Props> = ({ view }) => {
   return (
-    <div className="w-full h-full px-3 py-4 shadow-lg rounded-b-md rounded-tr-md bg-rhyth-light-blue">
-      <div className="flex flex-col justify-center items-center gap-3 bg-white rounded-lg shadow-md py-3">
-        {view === "NEXT" ? (
-          <>
-            <Image src="/icons/analysis.png" layout="constrained" width={80} height={80} alt="analysis icon" />
-            <span className="font-bold text-center p-2 text-rhyth-dark-blue">
-              新しい習慣がどのくらい身に付いたか
-              <br />
-              統計レポートで確認しましょう！
-            </span>
-          </>
-        ) : (
-          <>
-            <Image src="/icons/shuttle.png" layout="constrained" width={80} height={80} alt="shuttle icon" />
-            <span className="font-bold text-center p-2 text-rhyth-dark-blue">
-              終了したクエストはまだありません。
-              <br />
-              今日も一日頑張りましょう！
-            </span>
-          </>
-        )}
-      </div>
+    <div className="flex flex-col justify-center items-center gap-3 w-full h-full p-3 bg-white rounded-lg shadow-md">
+      {view === "NEXT" ? (
+        <>
+          <Image src="/icons/analysis.png" layout="constrained" width={80} height={80} alt="analysis icon" />
+          <span className="font-bold text-center p-2 text-rhyth-dark-blue">
+            新しい習慣がどのくらい身に付いたか
+            <br />
+            統計レポートで確認しましょう！
+          </span>
+        </>
+      ) : (
+        <>
+          <Image src="/icons/shuttle.png" layout="constrained" width={80} height={80} alt="shuttle icon" />
+          <span className="font-bold text-center p-2 text-rhyth-dark-blue">
+            終了したクエストはまだありません。
+            <br />
+            今日も一日頑張りましょう！
+          </span>
+        </>
+      )}
     </div>
   );
 };
