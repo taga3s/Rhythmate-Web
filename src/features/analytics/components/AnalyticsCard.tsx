6fd0181fdfc3639e7d2894fdd1858e29e8b39568
taga3s <bepozurapu274@gmail.com>
2024-10-27 12:48:43 +0900

    refactor /analytics

--- src/features/analytics/components/AnalyticsCard.tsx
+++ src/features/analytics/components/AnalyticsCard.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import { toRhythTextColor } from "../../common/utils";
 
 type Props = {
   title: string;
@@ -9,11 +10,8 @@ type Props = {
 
 export const AnalyticsCard: FC<Props> = ({ title, data, color, isRate }) => {
   return (
-    <div
-      className="text-lg border-[6px] w-full px-2 py-4 bg-white text-rhyth-dark-blue font-bold rounded-lg shadow"
-      style={{ borderColor: `${color}` }}
-    >
-      <span className="block text-center text-sm">{title}</span>
+    <div className="text-lg border-2 w-full px-2 py-4 bg-white text-rhyth-dark-blue rounded-lg shadow">
+      <span className={`block text-center text-sm font-bold ${toRhythTextColor(color)}`}>{title}</span>
       <span className="block text-center mt-4 text-4xl font-bold font-cp-font">
         {data}
         {isRate && <span>%</span>}
