214cb60026f6d1db3e0eaff403b5eb8d358cb0e4
taga3s <bepozurapu274@gmail.com>
2024-11-01 20:52:52 +0900

    fix interface of factory layer

--- src/api/tag/factory.ts
+++ src/api/tag/factory.ts
@@ -1,24 +1,25 @@
-import { tagRepository } from "./repository";
+import { toTag } from "./model";
+import { TagRepository, tagRepository } from "./repository";
 import type { CreateTagParams, DeleteTagParams, UpdateTagParams } from "./type";
 
 export const createFactory = () => {
-  const repository = tagRepository;
+  const repository: TagRepository = tagRepository;
 
   return {
     listTags: async () => {
       const response = await repository.list();
-      return response.tags;
+      return response.tags.map((tag) => toTag(tag));
     },
     createTag: async (CreateTagParams: CreateTagParams) => {
       const response = await repository.create(CreateTagParams);
-      return response;
+      return toTag(response);
     },
     deleteTag: async (DeleteTagParams: DeleteTagParams) => {
       await repository.destroy(DeleteTagParams);
     },
     updateTag: async (UpdateTagParams: UpdateTagParams) => {
       const response = await repository.update(UpdateTagParams);
-      return response;
+      return toTag(response);
     },
   };
 };
